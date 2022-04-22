5cec1a43514b0c68e86c10c3661f3db94ad35452
haemiii <73984822+haemiii@users.noreply.github.com>
2022-04-22 10:05:36 +0900

    session, db 유저 설정

--- src/controllers/backController.js
+++ src/controllers/backController.js
@@ -1,8 +1,26 @@
+import User from "../models/User.js";
+
 export const home = (req, res) => {
   return res.send("Home");
 };
 export const success = async (req, res) => {
-  return res.send(`Hi ${req.user.displayName}`);
+  const { email, name, picture } = req.user._json;
+
+  res.locals.photos = picture;
+  res.locals.name = name;
+
+  let user = await User.findOne({ email });
+  if (!user) {
+    console.log("No user");
+    user = await User.create({
+      avatarUrl: picture,
+      name,
+      email,
+    });
+  }
+  req.session.loggedIn = true;
+  req.session.user = user;
+  return res.redirect("/");
 };
 export const fail = (req, res) => {
   return res.send("You Failed to log in!");
--- src/server.js
+++ src/server.js
@@ -1,15 +1,44 @@
 import express from "express";
 import dotenv from "dotenv";
+import cors from "cors";
+import session from "express-session";
+import MongoStore from "connect-mongo";
+
 import passport from "passport";
 
 import backRouter from "./routers/backRouter.js";
 
+import { localsMiddleware } from "./middleware.js";
 import "./passport.js";
 import "./db.js";
 
 const app = express();
 
 dotenv.config();
+
+app.use(cors());
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(
+  session({
+    secret: process.env.COOKIE_SECRET,
+    resave: false,
+    saveUninitialized: false,
+    store: MongoStore.create({ mongoUrl: process.env.DB_URL }),
+  })
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.use(localsMiddleware);
+app.use((req, res, next) => {
+  res.header("Cross-Origin-Embedder-Policy", "credentialless");
+  res.header("Cross-Origin-Opener-Policy", "same-origin");
+  next();
+});
 
 app.get(
   "/google",
