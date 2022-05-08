0f6e62b23bfa8f0a32aacf9b78358265b45e6d4d
haemiii <73984822+haemiii@users.noreply.github.com>
2022-05-08 23:56:15 +0900

    comment 관련 수정

--- src/controllers/writingController.js
+++ src/controllers/writingController.js
@@ -194,7 +194,8 @@ export const infoSee = async (req, res) => {
     return res.send("Nothing");
   }
   console.log(writing.comments);
-  if (writing.comments == []) {
+  if (writing.comments === []) {
+    console.log("no comments");
     const infowriting = await InfoWriting.findById(id).populate("owner");
     return res.send(infowriting);
   }
