4b1cb321df3b83207b362ebe894c425eff2bf95b
haemiii <73984822+haemiii@users.noreply.github.com>
2022-05-09 14:45:57 +0900

    console

--- src/controllers/commentController.js
+++ src/controllers/commentController.js
@@ -230,6 +230,7 @@ export const clickHeart = async (req, res) => {
   } else {
     console.log(`defined ${createinfoheart}`);
     console.log(`defined ${createinfoheart[0].owner}`);
+    console.log(`user._id ${user._id}`);
 
     if (user._id in createinfoheart[0].onwer) {
     } else {
